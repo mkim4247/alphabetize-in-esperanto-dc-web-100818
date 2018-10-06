342b8ad717c5356fcb17b80bfb9eb0cd13b4c0b4
Michael Kim <m.kim4247@gmail.com>
2018-10-06 17:07:56 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,7 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
 
   arr.sort_by do |word|
-    word.split(" ").map do |letter|
+    word.split("").map do |letter|
       ESPERANTO_ALPHABET.index(letter)
     end 
   end 
